42c93cd9794a2a8ce03f0185bd5e03845ce9a0e8
learndroid <chris@learndroid.net>
2015-09-01 21:34:19 +0200

    Made Activity interactive by letting user enter her name.

--- app/src/main/java/net/learndroid/hellodroid/HelloActivity.java
+++ app/src/main/java/net/learndroid/hellodroid/HelloActivity.java
@@ -1,19 +1,35 @@
 package net.learndroid.hellodroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class HelloActivity extends AppCompatActivity {
 
+    // Attributes
+    //--------------------------------------------------------------------------
+    private EditText nameEditText;
+    private Button okButton;
+    private TextView helloWorldTextView;
+
+    // Life-cycle
+    //--------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_hello);
-        TextView helloWorldTextView = (TextView) findViewById(R.id.helloWorldTextView);
-        helloWorldTextView.setText("Hello Java!");
+        this.findViews();
+    }
+
+    private void findViews() {
+        this.nameEditText = (EditText) this.findViewById(R.id.nameEditText);
+        this.okButton = (Button) this.findViewById(R.id.okButton);
+        this.helloWorldTextView = (TextView) this.findViewById(R.id.helloWorldTextView);
     }
 
     @Override
@@ -22,6 +38,32 @@ public class HelloActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HelloActivity.this.onOkButtonClicked();
+            }
+        });
+    }
+
+    private void onOkButtonClicked() {
+        this.helloWorldTextView.setText(this.getString(
+                        R.string.hello_format,
+                        this.nameEditText.getText().toString())
+        );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.okButton.setOnClickListener(null);
+    }
+
+    // UI events
+    //--------------------------------------------------------------------------
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
