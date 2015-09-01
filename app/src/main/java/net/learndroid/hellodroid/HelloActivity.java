3953b1bf5e5b6f306262c8ed567cdf8031c8cb96
learndroid <chris@learndroid.net>
2015-09-01 21:07:22 +0200

    Renamed HomeActivity to HelloActivity

--- app/src/main/java/net/learndroid/hellodroid/HelloActivity.java
+++ app/src/main/java/net/learndroid/hellodroid/HelloActivity.java
@@ -4,27 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HelloActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello);
+        this.setContentView(R.layout.activity_hello);
+        TextView helloWorldTextView = (TextView) findViewById(R.id.helloWorldTextView);
+        helloWorldTextView.setText("Hallo, Java!");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        this.getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
